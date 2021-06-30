cdd9d92b8ebdd0971b730987beffe7a28ed95153
Ilya Zhukov <ilyazhukovp@gmail.com>
2021-06-30 20:40:00 +0300

    shutdown

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -6,18 +6,19 @@ public class CreditCalculator {
     }
 
     public double monthlyPayment(double money, double time) {
+        return (((((15 / 100) * time) * money) + money) / time) / 12;
 
 
 
     }
 
     public double refundAmount(double money, double time) {
-
+        return (((((15 / 100) * time) * money) + money));
 
     }
 
     public double overpayment(double money, double time) {
-
+        return ((15 / 100) * time) * money;
 
     }
 }
